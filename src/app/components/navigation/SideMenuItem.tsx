eaa3cc1defc4f1ce70b223f7f0a8d501ef564c85
Shameera17 <shameera.carrim17@gmail.com>
2024-12-06 08:00:50 +0530

    modified SideMenuItem comp

--- src/app/components/navigation/SideMenuItem.tsx
+++ src/app/components/navigation/SideMenuItem.tsx
@@ -24,8 +24,8 @@ export const SideMenuItem: React.FC<SideMenuItemProps> = ({
           href={url}
           className={`flex items-center gap-2 py-5 px-3 rounded transition-all duration-200 ease-linear font-semibold text-base ${
             isActive
-              ? "bg-[#0359E0] text-white hover:bg-[#0359E0] hover:text-white"
-              : "bg-[#F6F6F6] text-gray-700 hover:text-slate-500 hover:bg-slate-200"
+              ? "bg-[#0359E0] text-white hover:!bg-[#0249C4] hover:!text-white"
+              : "bg-[#F6F6F6] text-gray-700 hover:text-gray-800 hover:bg-gray-200"
           }`}
         >
           <Icon className="h-6 w-6" />
